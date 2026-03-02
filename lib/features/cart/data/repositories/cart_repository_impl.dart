2642eab57513105696aa612ae8532ce0bd16f9c2
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:12:57 +0700

    implemen kontrak kontak yang ada di cart repository

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -1,5 +1,20 @@
+import 'package:catalog_state_management/features/cart/domain/entities/product.dart';
 import 'package:catalog_state_management/features/cart/domain/repositories/cart_repository.dart';
 
 class CartRepositoryImpl implements CartRepository {
-  
+  final List<Product> _items = [];
+
+  @override
+  List<Product> getCartItems() => List.unmodifiable(_items);
+
+  @override
+  void addItem(Product product) => _items.add(product);
+
+  @override
+  void removeAll() {
+    _items.clear();
+  }
+
+  @override
+  bool isItemInCart(String productId) => _items.any((p) => p.id == productId);
 }
