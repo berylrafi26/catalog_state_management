e0622ca2b5bb2e45c7c1f9b498e299de30299436
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:02:37 +0700

    membuat routing untuk mengarahkan ke cataloh dan cart page

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,3 +1,13 @@
+import 'package:catalog_state_management/features/cart/presentation/pages/cart_page.dart';
+import 'package:catalog_state_management/features/cart/presentation/pages/catalog_page.dart';
+import 'package:flutter/material.dart';
+
 class AppRoute {
-  
+  static const String catalog = '/';
+  static const String cart = '/cart';
+
+  static Map<String, WidgetBuilder> get routes => {
+    catalog: (context) => CatalogPage(),
+    cart: (context) => CartPage(),
+  };
 }
