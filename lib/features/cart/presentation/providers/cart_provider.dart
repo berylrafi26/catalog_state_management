d8daa151a6bcee9625fed080d8f9caa97a571d7d
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:14:32 +0700

    membuat class cartprovider

---
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -0,0 +1,8 @@
+import 'package:catalog_state_management/features/cart/domain/repositories/cart_repository.dart';
+import 'package:flutter/material.dart';
+
+class CartProvider extends ChangeNotifier {
+  final CartRepository cartRepository;
+
+  
+}
