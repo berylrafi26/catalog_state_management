eeadb89bddd472907b776b60c23141c3c9d7b04c
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:04:32 +0700

    membuat constructor untuk product

--- lib/features/cart/domain/entities/product.dart
+++ lib/features/cart/domain/entities/product.dart
@@ -2,4 +2,10 @@ class Product {
   final String id;
   final String name;
   final String price;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
 }
