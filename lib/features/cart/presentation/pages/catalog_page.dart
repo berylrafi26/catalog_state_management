884ab478faa5377979c8b3d56440069279a9e7c1
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:21:37 +0700

    tambah menu

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,3 +1,4 @@
+import 'package:catalog_state_management/features/cart/domain/entities/product.dart';
 import 'package:flutter/material.dart';
 
 class CatalogPage extends StatelessWidget {
@@ -5,6 +6,13 @@ class CatalogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final products = [
+      Product(id: '1', name: 'Espresso', price: '18000'),
+      Product(id: '2', name: 'Cappuccino', price: '25000'),
+      Product(id: '3', name: 'Caramel Latte', price: '28000'),
+      Product(id: '4', name: 'Matcha Latte', price: '27000'),
+      Product(id: '5', name: 'French Fries', price: '20000'),
+    ];
     return const Placeholder();
   }
 }
