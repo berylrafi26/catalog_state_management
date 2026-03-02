f11615f43274003e51a97eb3c37dcbb42bbd87f6
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:34:59 +0700

    insisiasi route, kasih title, dan hilangin debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catalog_state_management/core/routes/app_route.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      initialRoute: AppRoute.catalog,
+      routes: AppRoute.routes,
+      title: 'Kafe',
+      debugShowCheckedModeBanner: false,
     );
   }
 }
