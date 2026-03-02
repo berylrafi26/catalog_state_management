b14d7f50e64ab37fd8bb34b3a8583cbf8efd3961
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:25:40 +0700

    membuat tampilan catalog kafe

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,4 +1,6 @@
+import 'package:catalog_state_management/core/routes/app_route.dart';
 import 'package:catalog_state_management/features/cart/domain/entities/product.dart';
+import 'package:catalog_state_management/features/cart/presentation/widgets/add_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class CatalogPage extends StatelessWidget {
@@ -13,6 +15,27 @@ class CatalogPage extends StatelessWidget {
       Product(id: '4', name: 'Matcha Latte', price: '27000'),
       Product(id: '5', name: 'French Fries', price: '20000'),
     ];
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kafe'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () => Navigator.pushNamed(context, AppRoute.cart),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            trailing: AddButtonWidget(product: products[index]),
+          );
+        },
+      ),
+    );
   }
 }
