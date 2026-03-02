121dca06a4c8dc9ce607903e2b9d24191cff82a1
berylrafi26 <berylrafi3@gmail.com>
2026-03-02 20:18:30 +0700

    membuat provider

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,8 +1,28 @@
+import 'package:catalog_state_management/features/cart/domain/entities/product.dart';
 import 'package:catalog_state_management/features/cart/domain/repositories/cart_repository.dart';
 import 'package:flutter/material.dart';
 
 class CartProvider extends ChangeNotifier {
-  final CartRepository cartRepository;
+  final CartRepository _repository;
 
-  
+  CartProvider({
+    required CartRepository repository
+  }) : _repository = repository;
+
+  List<Product> get items => _repository.getCartItems();
+
+  void addItem(Product product){
+    _repository.addItem(product);
+    notifyListeners();
+  }
+
+  void removeAll(){
+    _repository.removeAll();
+    notifyListeners();
+  }
+
+  bool isInCart(String productId) =>
+    _repository.isItemInCart(productId);
+
+
 }
